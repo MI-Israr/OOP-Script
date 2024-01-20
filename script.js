e1410f86af4b30787808ca46ee437534f6f968f8
MI-Israr <m.israr1228@gmail.com>
2024-01-20 12:36:11 +0500

    modified files

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const Person = function (firstname, birthYear) {
+const Users = function (firstname, birthYear) {
   (this.firstname = firstname), (this.birthYear = birthYear);
 };
 
-const jonas = new Person("jonas", 1991);
-const smith = new Person("smith", 1998);
-const harry = new Person("harry", 2003);
+const jonas = new Users("jonas", 1991);
+const smith = new Users("smith", 1998);
+const harry = new Users("harry", 2003);
 
 console.log(jonas, smith, harry);

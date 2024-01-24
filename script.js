fbbcee7ae1ca926bc96705decd90044a865c6c1a
MI-Israr <m.israr1228@gmail.com>
2024-01-24 15:42:31 +0500

    comented code

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 "use strict";
 
-const Person = function (firstName, birthYear) {
-  (this.firstName = firstName), (this.birthYear = birthYear);
-};
+// const Person = function (firstName, birthYear) {
+//   (this.firstName = firstName), (this.birthYear = birthYear);
+// };
 
-const jonas = new Person("jonas", 1991);
-const smith = new Person("smith", 1998);
-const harry = new Person("harry", 2003);
+// const jonas = new Person("jonas", 1991);
+// const smith = new Person("smith", 1998);
+// const harry = new Person("harry", 2003);
 
-console.log(jonas, smith, harry);
+// console.log(jonas, smith, harry);
 
-Person.prototype.calcAge = function () {
-  console.log(2024 - this.birthYear);
-};
+// Person.prototype.calcAge = function () {
+//   console.log(2024 - this.birthYear);
+// };
 
-jonas.calcAge();
-smith.calcAge();
+// jonas.calcAge();
+// smith.calcAge();
 
-console.log(jonas.__proto__);
-console.log(jonas.__proto__ === Person.prototype);
+// console.log(jonas.__proto__);
+// console.log(jonas.__proto__ === Person.prototype);
 
-console.log(Person.prototype.isPrototypeOf(jonas));
-console.log(Person.prototype.isPrototypeOf(smith));
-console.log(Person.prototype.isPrototypeOf(Person));
+// console.log(Person.prototype.isPrototypeOf(jonas));
+// console.log(Person.prototype.isPrototypeOf(smith));
+// console.log(Person.prototype.isPrototypeOf(Person));
 
 // .prototype is actually the .prototypeOfLinkedObjects
 
-Person.prototype.species = "Homo Sapiens";
+// Person.prototype.species = "Homo Sapiens";
 
-console.log(jonas.species, smith.species, harry.species);
-console.log(jonas.hasOwnProperty("firstName"));
-console.log(jonas.hasOwnProperty("species"));
+// console.log(jonas.species, smith.species, harry.species);
+// console.log(jonas.hasOwnProperty("firstName"));
+// console.log(jonas.hasOwnProperty("species"));
